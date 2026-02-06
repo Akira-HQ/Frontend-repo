8c7c5008d2b018d9d009f6a0c5260b7a89c18ee0
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2026-02-06 21:17:36 +0100

    added my footer routes

--- components/landing/Pricings.tsx
+++ components/landing/Pricings.tsx
@@ -56,6 +56,7 @@ export const PricingCard: React.FC<PricingCardProps> = ({
 
   return (
     <motion.div
+      id="pricings"
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
